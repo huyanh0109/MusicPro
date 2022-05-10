fcfe7d3149f0e6d1a702ccaabd09c0019c93fa76
huyanh0109 <huyanh0109@gmail.com>
2022-05-10 14:23:25 +0700

    update 10/05

---
+++ app/src/main/java/com/example/musicpro/AllMusic.java
@@ -0,0 +1,14 @@
+package com.example.musicpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_music);
+    }
+}
---
+++ app/src/main/java/com/example/musicpro/DetailMusic.java
@@ -0,0 +1,14 @@
+package com.example.musicpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class DetailMusic extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_music);
+    }
+}
